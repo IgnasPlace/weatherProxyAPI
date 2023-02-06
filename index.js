01b8f0ab31d912a69ef81841138e174c12152571
Ignas <76262712+IgnasPlace@users.noreply.github.com>
2023-02-06 12:26:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 // Rate limiting
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 20
+    max: 100
 })
 app.use(limiter);
 app.set('trust proxy', 1)
