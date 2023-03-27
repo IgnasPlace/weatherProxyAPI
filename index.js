762b0d18f5f905954dc862a55d09d957dfe4228f
IgnasPlace <ignasplace@gmail.com>
2023-03-27 14:49:53 +0200

    changed website name for cors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const rateLimit = require('express-rate-limit');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,22 +9,24 @@ const app = express();
 
 // Rate limiting
 const limiter = rateLimit({
-    windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 40
-})
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 40,
+});
 app.use(limiter);
-app.set('trust proxy', 1)
+app.set("trust proxy", 1);
 
 // Enable cors
-app.use(cors({
-    origin: ['https://ignasplace-weather.netlify.app', 'http://localhost:5173']
-}));
+app.use(
+  cors({
+    origin: ["https://weather.ignasplace.com/", "http://localhost:5173"],
+  })
+);
 // app.use(cors());
 
 // Set static folder
 // app.use(express.static('public'))
 
 // Routes
-app.use('/api', require('./routes'))
+app.use("/api", require("./routes"));
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
